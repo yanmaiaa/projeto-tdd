f9d8725b9b89852129c503e69eb8ed748e578e32
Yan Maia <yandamasceno01@gmail.com>
2022-12-17 02:41:13 -0300

    test: garantir SignUpController retorne um erro se email nao for preenchido

--- node-api/src/presentation/controllers/signup.spec.ts
+++ node-api/src/presentation/controllers/signup.spec.ts
@@ -16,3 +16,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Sem parametro: nome')) //comparar objetos com o toBe não funciona pois ele compara "ponteiro" a "ponteiro", necessitando que os dois fossem idênticos, logo, trocaremos o toBe para comparar só os valores do objeto e ver se são iguais
   })
 })
+
+describe('SignUp Controller', () => {  
+  test('Se não receber um email no body da requisição tipo post  erro 400', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'qualquer_nome',
+        password: 'qualquer_password',
+        passwordConfirmation: 'qualquer_password',
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Sem parametro: email')) 
+  })
+})
