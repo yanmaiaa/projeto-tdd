789c242570c1e361aa6db58012192c5db932cdf9
Yan Maia <yandamasceno01@gmail.com>
2022-12-18 00:59:19 -0300

    refactor: implementacao do SignUpController conforme protocolo

--- node-api/src/presentation/controllers/SignUp.ts
+++ node-api/src/presentation/controllers/SignUp.ts
@@ -1,8 +1,9 @@
+import { Controller } from './../protocols/controller';
 import {HttpResponse, HttpRequest} from '../protocols/http';
 import {MissingParamError} from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 
-export class SignUpController{
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse{
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
