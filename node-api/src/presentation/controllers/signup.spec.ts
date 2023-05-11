28db6c71482776baef69907e3ec9b0dd7383f668
Yan Maia <yandamasceno01@gmail.com>
2023-05-11 01:10:45 -0300

    test: garantindo SignUpController retorna 400 se um email for invalido

--- node-api/src/presentation/controllers/signup.spec.ts
+++ node-api/src/presentation/controllers/signup.spec.ts
@@ -1,14 +1,29 @@
 import { InvalidParamError } from './../errors/invalid-param-error';
 import { SignUpController } from './SignUp';
 import {MissingParamError} from '../errors/missing-param-error';
+import { EmailValidator } from '../protocols/email-validator';
 
-const makeSut = (): SignUpController => {
-  return new SignUpController(); //quando criarmos novas dependencias, não precisaremos mudar nenhum dos construtores dos testes
+interface SutTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator{ //injeção mockada
+    isValid(email: string): boolean{
+      return true;
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub();
+  const sut = new SignUpController(emailValidatorStub); //quando criarmos novas dependencias, não precisaremos mudar nenhum dos construtores dos testes
+  return {
+    sut,
+    emailValidatorStub
+  }
 }//factory
 
 describe('SignUp Controller', () => {  
   test('Se não enviar um nome no body da requisição tipo post retorna o erro 400', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'qualquer_nome@mail.com',
@@ -24,7 +39,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {  
   test('Se não receber um email no body da requisição tipo post  erro 400', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'qualquer_nome',
@@ -40,7 +55,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {  
   test('Se não tiver um password deverá retornar 400', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'qualquer_nome',
@@ -56,7 +71,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {  
   test('Se não tiver um passwordConfirmation deverá retornar 400', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'qualquer_nome',
@@ -72,7 +87,8 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {  
   test('Se o email for invalido deverá retornar 400', () => {
-    const sut = makeSut();
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
     const httpRequest = {
       body: {
         name: 'qualquer_nome',
